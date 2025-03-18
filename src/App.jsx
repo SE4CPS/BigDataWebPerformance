5ac226016e1b9776da46463738248fad52fd323c
Samrudh Uchil <rtrsamrudhuchil@gmail.com>
2025-03-17 23:26:55 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ToDoList from './ToDoList.jsx';
+import React from './ToDoList.jsx';
+
+function App() {
+
+  return(<ToDoList/>)
+}
+
+export default App;
---
+++ src/ToDoList.jsx
@@ -0,0 +1,107 @@
+
+import React, { useState } from 'react';
+
+function ToDoList(){
+
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event){
+        setNewTask(event.target.value)
+        // alert(1);
+    } 
+
+    function addTask(){
+
+        if(newTask.trim() !== ""){
+            setTasks (t => [...t, newTask]);
+            setNewTask ("");
+        }
+    }
+
+    function deleteTask(index){
+
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
+        console.log(tasks);
+
+    }
+
+    function moveTaskUp(index){
+
+        if(index>0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index],updatedTasks[index - 1]] = [updatedTasks[index - 1],updatedTasks[index]]
+            setTasks(updatedTasks);
+        }
+    }
+
+    function moveTaskDown(index){
+
+        if(index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index],updatedTasks[index + 1]] = [updatedTasks[index + 1],updatedTasks[index]]
+            setTasks(updatedTasks);
+        }
+        // if a==0?print:
+    }
+
+    function callConsoleLog(){
+        console.log(tasks);
+    }
+
+    return(
+    <div className='to-do-list'>
+        <title>ToDo List</title>
+        <h1>To-Do-List</h1>
+
+        <div>
+            <input 
+            type = "text"
+            placeholder='Enter a Task..'
+            value={newTask}
+            onChange={handleInputChange}
+            />
+
+            <button
+                className = "add-button"
+                onClick = {addTask}>
+                Add
+            </button>
+            <button
+                className='move-button'
+                onClick={() => callConsoleLog()}>
+                    LOG  
+            </button>
+        </div>
+
+        <ol>
+            {tasks.map((task, index) => 
+                <li key={index}>
+                    <span className='text'>{task}</span>
+                    <button
+                        className='delete-button'
+                        onClick = {() => deleteTask(index)
+                        }>
+                            Delete 
+                    </button>
+                    <button
+                        className='move-button'
+                        onClick={() => moveTaskUp(index)}>
+                            🔼
+                    </button>
+                    <button
+                        className='move-button'
+                        onClick={() => moveTaskDown(index)}>
+                            🔽   
+                    </button>
+                    
+                </li>
+            )}
+        </ol>
+
+
+    </div>
+    );
+}
+export default ToDoList;
